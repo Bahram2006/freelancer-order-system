73e53bf653c85f364d121e20578bb23fca0152bb
Bahram2006 <yardam034@gmail.com>
2026-04-18 12:19:09 +0500

    refactor routes with protected access and clean structure

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,22 +1,58 @@
 import { Routes, Route } from "react-router-dom";
-import EditOrder from "../pages/orders/EditOrder";
 
-import Login from "../pages/auth/Login";
 import Dashboard from "../pages/dashboard/Dashboard";
 import OrdersList from "../pages/orders/OrdersList";
 import CreateOrder from "../pages/orders/CreateOrder";
+import EditOrder from "../pages/orders/EditOrder";
+
+import Login from "../pages/auth/Login";
 import Register from "../pages/auth/Register";
 
+import ProtectedRoute from "./ProtectedRoute";
 
 export default function AppRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<Dashboard />} />
+      {/* 🔓 PUBLIC ROUTES */}
       <Route path="/login" element={<Login />} />
-      <Route path="/orders" element={<OrdersList />} />
-      <Route path="/create-order" element={<CreateOrder />} />
-      <Route path="/orders/edit/:id" element={<EditOrder />} />
       <Route path="/register" element={<Register />} />
+
+      {/* 🔐 PROTECTED ROUTES */}
+      <Route
+        path="/"
+        element={
+          <ProtectedRoute>
+            <Dashboard />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/orders"
+        element={
+          <ProtectedRoute>
+            <OrdersList />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/create-order"
+        element={
+          <ProtectedRoute>
+            <CreateOrder />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/orders/edit/:id"
+        element={
+          <ProtectedRoute>
+            <EditOrder />
+          </ProtectedRoute>
+        }
+      />
     </Routes>
   );
 }
